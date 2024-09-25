f880553ac337d2db3528f17507f741fe03bf00bd
najwazetti <rivanniaaritha@gmail.com>
2024-09-25 10:58:47 +0800

    review 2

--- lib/main.dart
+++ lib/main.dart
@@ -31,54 +31,88 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
-        ),
-        body: Container(
-          width: 200,
-          height: 200,
-          padding: const EdgeInsets.all(9.0),
-          // color: Colors.black,
-          decoration: BoxDecoration(
-              border: Border.all(
-            width: 3,
-            color: Colors.orange,
-          )),
-          child: const Text(
-            'NAJWA FELIRRRRRRRRRRRAAAAAAAAAA ZETTTTTTTTTTTTTIIIIIIIIIII',
-            style: TextStyle(
-                fontWeight: FontWeight.bold, color: Colors.deepPurple),
-          ),
-        )
+    final titles = ['Bike', 'Boat', 'Bus'];
+    final icons = [
+      Icons.directions_bike,
+      Icons.directions_boat,
+      Icons.directions_bus
+    ];
 
-        // body: Center(
-        //   child: Column(
-        //     children: [
-        //       Container(
-        //         width: 100,
-        //         height: 100,
-        //         color: Colors.blueAccent,
-        //       ),
-        //       Container(
-        //         width: 100,
-        //         height: 100,
-        //         color: Colors.deepOrange,
-        //       )
-        //     ],
-        //   ),
-        // )
-        );
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("List View"),
+        backgroundColor: Colors.deepOrange[300],
+      ),
+      body: ListView.builder(
+        itemCount: titles.length,
+        itemBuilder: (context, nomor) {
+          return Card(
+              child: ListTile(
+                  leading: Icon(icons[nomor]),
+                  title: Text(titles[nomor]),
+                  onTap: () {
+                    setState(() {
+                      titles.removeAt(nomor);
+                    });
+                  }));
+        },
+      ),
+      // children: const [
+      //   ListTile(
+      //     leading: Icon(Icons.sunny),
+      //     title: Text("Sun"),
+      //     trailing: Icon(Icons.keyboard_arrow_right),
+      //   ),
+      //   ListTile(
+      //     leading: Icon(Icons.brightness_3),
+      //     title: Text("Moon"),
+      //     trailing: Icon(Icons.keyboard_arrow_right),
+      //   ),
+      //   ListTile(
+      //     leading: Icon(Icons.star),
+      //     title: Text("Star"),
+      //     trailing: Icon(Icons.keyboard_arrow_right),
+      //   )
+      // ],
+
+      // GridView(
+      //   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+      //     crossAxisCount: 4,
+      //   ),
+      //   children: [
+      //     Container(
+      //       width: 100,
+      //       height: 250,
+      //       margin: const EdgeInsets.only(bottom: 10),
+      //       decoration: const BoxDecoration(
+      //         color: Colors.red,
+      //         image: DecorationImage(
+      //           image: AssetImage("jua.jpg"),
+      //         ),
+      //       ),
+      //     ),
+      //     Container(
+      //       width: 100,
+      //       height: 250,
+      //       margin: const EdgeInsets.only(bottom: 10),
+      //       color: Colors.blue,
+      //     ),
+      //     Container(
+      //       width: 100,
+      //       height: 250,
+      //       margin: const EdgeInsets.only(bottom: 10),
+      //       color: Colors.black,
+      //     ),
+      //     Container(
+      //       width: 100,
+      //       height: 250,
+      //       margin: const EdgeInsets.only(bottom: 10),
+      //       color: Colors.yellow,
+      //     ),
+      //   ],
+      // ),
+    );
   }
 }
